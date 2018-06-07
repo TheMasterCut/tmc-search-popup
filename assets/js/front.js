f7dc40faa917f1f69da2deb334931b90beee6485
themastercut <hello@themastercut.co>
2018-06-07 10:18:21 +0200

    Refactor: Elems names.

--- assets/js/front.js
+++ assets/js/front.js
@@ -3,11 +3,11 @@ jQuery( document ).ready( function( $ ) {
     var popup = {
 
         'elems':            {
-            'popupRootEl':      $( '#tmc_sp_root' ),        //  Popup main object.
-            'popupCloseEls':    $( '#tmc_sp_close' ),       //  Close buttons.
-            'popupOpenEls':     $( '#adminbar-search' ),    //  Open buttons.
-            'popupFormEl':      $( '#tmc_sp_form' ),        //  Form.
-            'popupResultsEl':   $( '#tmc_sp_results' )      //  Results div.
+            "rootEl":      $( '#tmc_sp_root' ),        //  Popup main object.
+            "closeEls":    $( '#tmc_sp_close' ),       //  Close buttons.
+            "openEls":     $( '#adminbar-search' ),    //  Open buttons.
+            "formEl":      $( '#tmc_sp_form' ),        //  Form.
+            "resultsEl":   $( '#tmc_sp_results' )      //  Results div.
         },
 
         /**
@@ -17,22 +17,22 @@ jQuery( document ).ready( function( $ ) {
          */
         init:               function() {
 
-            if( ! popup.elems.popupRootEl ){
+            if( ! popup.elems.rootEl ){
                 console.log( 'Search Popup TMC could not initialize elements.' );
                 return;
             }
 
-            popup.elems.popupOpenEls.click( function( event ) {
+            popup.elems.openEls.click( function(event ) {
                 event.preventDefault();
                 popup.activatePopup();
             } );
 
-            popup.elems.popupCloseEls.click( function( event ) {
+            popup.elems.closeEls.click( function(event ) {
                 event.preventDefault();
                 popup.deactivatePopup();
             } );
 
-            popup.elems.popupFormEl.submit( function( event ) {
+            popup.elems.formEl.submit( function(event ) {
                 event.preventDefault();
                 popup.sendForm();
             } );
@@ -46,22 +46,22 @@ jQuery( document ).ready( function( $ ) {
          */
         activatePopup:      function() {
 
-            popup.elems.popupRootEl.addClass( 'is-active' );
+            popup.elems.rootEl.addClass( 'is-active' );
             $( 'body' ).addClass( 'noscroll' );
 
         },
 
         deactivatePopup:    function() {
 
-            popup.elems.popupRootEl.removeClass( 'is-active' );
+            popup.elems.rootEl.removeClass( 'is-active' );
             $( 'body' ).removeClass( 'noscroll' );
 
         },
 
         sendForm:           function() {
 
-            popup.elems.popupRootEl.addClass( 'has-results' );
-            popup.elems.popupResultsEl.html( '' );
+            popup.elems.rootEl.addClass( 'has-results' );
+            popup.elems.resultsEl.html( '' );
 
         }
 
